f4f1545cbe47996247faae31770db65c2eddbed5
Kamil Musiałkowski <k.musialkowski@gmail.com>
2021-09-29 17:25:11 +0200

    Initial commit

---
+++ src/main/java/pl/triggerpp/TriggerPpApplication.java
@@ -0,0 +1,13 @@
+package pl.triggerpp;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TriggerPpApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(TriggerPpApplication.class, args);
+    }
+
+}
---
+++ src/test/java/pl/triggerpp/TriggerPpApplicationTests.java
@@ -0,0 +1,13 @@
+package pl.triggerpp;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TriggerPpApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
